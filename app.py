ebffdc2a75c90be75258703427257c31e02d2f67
Gs-Dip <dibbo12345678ghosh@gmail.com>
2022-06-16 22:07:14 +0600

    authentication with auth0

---
+++ app.py
@@ -0,0 +1,30 @@
+# from flask import Flask,redirect,render_template,request,flash,session, url_for  
+
+
+
+# app=Flask(__name__)
+
+
+# @app.route('/')
+# def home():
+#     return render_template('index.html')
+
+# @app.route('/login')
+# def login():
+#     return render_template('login.html')    
+
+# @app.route('/usersignup')
+# def usersignup():
+#     return render_template('user_signup.html')    
+
+# @app.route('/adminsignup')
+# def adminsignup():
+#     return render_template('admin_signup.html')    
+
+
+
+
+
+
+# if __name__=='__main__':
+#     app.run(debug=True,port=7800)
